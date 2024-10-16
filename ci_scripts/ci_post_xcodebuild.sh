cbfb3d886f701e86b0eb81fffa76119490956599
Gru <ruslru376@gmail.com>
2024-10-17 04:05:54 +1000

    garbage

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -5,23 +5,23 @@
 #
 #  Created by Angelo on 22/08/2024.
 #
-
-set -e
-
-if [ ! -d "$CI_ARCHIVE_PATH" ]; then
-    echo "Archive does not exist, skipping Sentry upload"
-    exit 0
-fi
-
-export INSTALL_DIR=$PWD
-
-if [[ $(command -v sentry-cli) == "" ]]; then
-    echo "Installing Sentry CLI"
-    curl -sL https://sentry.io/get-cli/ | bash
-fi
-
-echo "Authenticate to Sentry"
-./sentry-cli login --auth-token $SENTRY_AUTH_TOKEN
-
-echo "Uploading dSYM to Sentry"
-./sentry-cli debug-files upload -o revolt -p apple-ios $CI_ARCHIVE_PATH --force-foreground
+#
+#set -e
+#
+#if [ ! -d "$CI_ARCHIVE_PATH" ]; then
+#    echo "Archive does not exist, skipping Sentry upload"
+#    exit 0
+#fi
+#
+#export INSTALL_DIR=$PWD
+#
+#if [[ $(command -v sentry-cli) == "" ]]; then
+#    echo "Installing Sentry CLI"
+#    curl -sL https://sentry.io/get-cli/ | bash
+#fi
+#
+#echo "Authenticate to Sentry"
+#./sentry-cli login --auth-token $SENTRY_AUTH_TOKEN
+#
+#echo "Uploading dSYM to Sentry"
+#./sentry-cli debug-files upload -o revolt -p apple-ios $CI_ARCHIVE_PATH --force-foreground
